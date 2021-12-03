6765c081b40fc872417397856a34f991887b7c08
PAN <pan910421@gmail.com>
2021-12-03 21:27:00 +0800

    day2

---
+++ src/Day02.kt
@@ -0,0 +1,58 @@
+package day02
+
+import readInput
+
+
+fun part1(input: List<String>):Int {
+    var horizontal = 0
+    var depth = 0
+    var i = 0
+    while (true) {
+        val (direction, position) = input[i].split(" ")
+        when(direction){
+            "forward" -> horizontal += position.toInt()
+            "up" -> depth -= position.toInt()
+            "down" -> depth += position.toInt()
+         }
+        i++
+        if (i > input.size - 1) break
+    }
+    return horizontal.times(depth)
+}
+
+fun part2(input: List<String>):Int {
+    var horizontal = 0
+    var depth = 0
+    var aim = 0
+    var i = 0
+    while (true) {
+        val (direction, position) = input[i].split(" ")
+        when(direction){
+            "forward" -> {
+                horizontal += position.toInt()
+                depth += aim * position.toInt()
+
+            }
+            "up" -> aim -= position.toInt()
+            "down" -> aim += position.toInt()
+        }
+        i++
+        if (i > input.size - 1) break
+    }
+    return horizontal.times(depth)
+}
+
+
+fun main() {
+    val testInput = readInput("Day02_test")
+
+    println(check(part1(testInput) == 150))
+    println(check(part2(testInput) == 900))
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+
+
+
+}
