5dfbc715b8c36dd9be8bb7ace379221f068f494c
PAN <pan910421@gmail.com>
2021-12-04 02:05:55 +0800

    day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -25,11 +25,64 @@ fun main() {
         return (total - result) * result
     }
     fun part2(input:List<String>):Int {
-        return 0
+        var oxygen = input
+        var CO2 = input
+        var index = 0
+        while (true) {
+            var oxygentemp = 0
+            var CO2temp = 0
+
+            for (str in oxygen) {
+                if (str[index] == '1') oxygentemp++ else oxygentemp--
+            }
+            for (str in CO2) {
+                if (str[index] == '1') CO2temp++ else CO2temp--
+            }
+            if (oxygen.size>1){
+                when {
+                    oxygentemp > 0 -> {
+                        oxygen = oxygen.filter { it[index] == '1' }
+                    }
+                    oxygentemp == 0 -> {
+                        oxygen = oxygen.filter { it[index] == '1' }
+                    }
+                    oxygentemp < 0 -> {
+                        oxygen = oxygen.filter { it[index] == '0' }
+                    }
+                }
+            }
+
+            if (CO2.size>1){
+                when {
+                    CO2temp > 0 -> {
+                        CO2 = CO2.filter{ it[index] == '0' }
+                    }
+                    CO2temp == 0 -> {
+                        CO2 = CO2.filter{ it[index] == '0' }
+                    }
+                    CO2temp < 0 -> {
+                        CO2 = CO2.filter{ it[index] == '1' }
+                    }
+                }
+            }
+            index++
+            if (index >= input[0].length) break
+        }
+        var result = 0
+        var result1 = 0
+        for (i in 0..input[0].lastIndex) {
+            if (oxygen[0][i] == '1') {
+                result += 2.0.pow(oxygen[0].lastIndex - i).toInt()
+            }
+            if (CO2[0][i] == '1') {
+                result1 += 2.0.pow(CO2[0].lastIndex - i).toInt()
+            }
+        }
+        return result * result1
     }
     val testInput = readInput("Day03_test")
     println(check(part1(testInput) == 198))
-//    println(check(part2(testInput) == 900))
+    println(check(part2(testInput) == 230))
 
     val input = readInput("Day03")
     println(part1(input))
